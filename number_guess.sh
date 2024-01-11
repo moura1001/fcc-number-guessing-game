1b13356dcae348a3ecc0a4074fef31a93d716976
moura1001 <gmouraneto01@gmail.com>
2024-01-11 23:24:28 +0000

    feat: keeps asking for input until user enters the secret number

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,8 @@
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 # generates a random number between 1 and 1000
 MAGIC_NUMBER=$(( RANDOM % 1000 + 1 ))
-ATUAL_GUESSING_ATTEMPTS=1
+echo "secret number: $MAGIC_NUMBER"
+ATUAL_GUESSING_ATTEMPTS=0
 IS_FIRST_GAME=false
 # max integer value
 PLAYER_BEST_GAME=$(( 2**63 - 1 ))
@@ -23,32 +24,51 @@ START_GAME() {
   fi
 
   PLAYER_INFO_RESULT=$(echo $PLAYER_INFO_RESULT | sed 's/ //g')
-  echo $PLAYER_INFO_RESULT | while IFS="|" read PLAYER_USERNAME GAMES_PLAYED BEST_GAME
-  do
-    if [[ $BEST_GAME -gt 0 && $BEST_GAME -lt $PLAYER_BEST_GAME ]]
-    then
-      PLAYER_BEST_GAME=$BEST_GAME
-    fi
+  IFS="|" read PLAYER_USERNAME GAMES_PLAYED BEST_GAME <<< $PLAYER_INFO_RESULT
+  
+  if [[ $BEST_GAME -gt 0 && $BEST_GAME -lt $PLAYER_BEST_GAME ]]
+  then
+    PLAYER_BEST_GAME=$BEST_GAME
+  fi
 
-    if $IS_FIRST_GAME
-    then
-      echo "Welcome, $PLAYER_USERNAME! It looks like this is your first time here."
-    else
-      echo "Welcome back, $PLAYER_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-    fi
+  if $IS_FIRST_GAME
+  then
+    echo "Welcome, $PLAYER_USERNAME! It looks like this is your first time here."
+  else
+    echo "Welcome back, $PLAYER_USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
 
-    PLAY $PLAYER_USERNAME $PLAYER_BEST_GAME
-  done
+  PLAY $PLAYER_USERNAME $PLAYER_BEST_GAME
 }
 
 PLAY() {
-  echo $1 $2
+  echo "Guess the secret number between 1 and 1000:"
+
+  USER_GUESS=0
+  while [[ $USER_GUESS != $MAGIC_NUMBER ]]
+  do
+    USER_GUESS=$(GET_USER_GUESS)
+    (( ATUAL_GUESSING_ATTEMPTS++ ))
+    echo "New user input: $USER_GUESS, attempt $ATUAL_GUESSING_ATTEMPTS"
+  done
+}
+
+GET_USER_GUESS() {
+  read USER_INPUT
+  
+  while [[ -z $USER_INPUT || ! $USER_INPUT =~ ^[0-9]+$ ]]
+  do
+    echo "That is not an integer, guess again:"
+    read USER_INPUT
+  done
+
+  echo $USER_INPUT
 }
 
 echo "Enter your username:"
 read USERNAME
 
-while [[ -z $USERNAME || ! $USERNAME =~ ^[a-zA-Z0-9]{2,22}$ ]]
+while [[ -z $USERNAME || ! $USERNAME =~ ^[a-zA-Z0-9_]{1,22}$ ]]
 do
   echo "Invalid input. Please, provide a valid username:"
   read USERNAME
